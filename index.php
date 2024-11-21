9be4a910a658be99563f89251c7eab38db631887
Alejandro <122414761+kikeEsponja@users.noreply.github.com>
2024-11-21 17:42:48 -0300

    Add files via upload
    first upload

---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="./src/img/logo.png">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.4/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/hint.css/2.7.0/hint.min.css">
+	<link href="https://fonts.cdnfonts.com/css/poppins" rel="stylesheet">
+    <link rel="stylesheet" href="./src/css/admin.css">
+    <title>Administración</title>
+</head>
+<body>
+    <div class="blue"></div>
+    <center>
+        <div class="cont-h1">
+			<img src="./src/img/logo.png" class="logo">
+            <!--<div class="logo">t</div>
+            <h1>wa</h1>-->
+        </div>
+        <a class="btn btn-primary" href="#logeo">Acceso</a>
+        <div class="cont-h2">
+            <h2 class="plano">START CODING</h2>
+        </div>
+        <h3>Horarios</h3>
+        <br>
+        <div id="mejores" class="mejores">
+            <?php
+                include "./src/js/config.php";
+                $sql = "SELECT fecha, hora, cupos FROM turnos WHERE cupos > 0 ORDER BY fecha, hora";
+
+                $result = mysqli_query($conn, $sql);
+
+                if(mysqli_num_rows($result) > 0){
+                    echo "<table>";
+                    echo "<tr>";
+                    echo "<th>Fecha</th>";
+                    echo "<th>Hora</th>";
+                    echo "<th>Cupos disponibles</th>";
+                    echo "</tr>";
+                    while($turno = mysqli_fetch_assoc($result)){
+                        echo "<tr>";
+                        echo "<td>". date("d-m-Y", strtotime($turno['fecha'])) . "</td>";
+                        echo "<td>". date("H:i", strtotime($turno['hora'])) ."</td>";
+                        echo "<td>". $turno['cupos'] . "</td>";
+                        echo "</tr>";
+                    }
+                    echo "</table>";
+                }else{
+                    echo "<p>No hay turnos disponibles</p>";
+                }
+            ?>
+        </div>
+        <br>
+        <div class="filtro">
+            <button type="button" class="btn btn-dark" id="filtro-html" name="filtro-vendedores">HTML</button>
+            <button type="button" class="btn btn-dark" id="filtro-css" name="filtro-setters">CSS</button>
+            <button type="button" class="btn btn-dark" id="filtro-js" name="filtro-closers">JavaScript</button>
+            <button type="button" class="btn btn-outline-success" id="mostrar-todo" name="mostrar-todo">Todos</button>
+        </div>
+        <br>
+        <div class="cont-texto">
+            <p class="texto">Puntaje de los perfiles.</p>
+        </div>
+    </center>
+    <br>
+    <br>
+    <div class="logeo">
+        <h4 style="text-decoration: underline;">Acceso para formadores y cursantes: </h4>
+        <button class="btn btn-success" id="logeo">Click Aquí</button>
+    </div>
+    <div class="red"></div>
+
+    <script src="./src/js/funciones.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.4.js" integrity="sha256-a9jBBRygX1Bh5lt8GZjXDzyOB+bWve9EiO7tROUtj/E=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+</body>
+</html>
